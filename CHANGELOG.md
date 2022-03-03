6f7e3c993ce17f547f6db979df66c3ceb06c8bae
Michael Benowitz <michaelbenowitz@nypl.org>
2022-03-03 16:27:01 -0500

    SFR-1432 Add CHANGELOG and cleanup

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## unreleased -- v0.0.1
+### Added
+- Initial commit of dockerized CCE/CCR Search API
--- api/prints/base.py
+++ api/prints/base.py
@@ -4,9 +4,8 @@ from model.cce import CCE
 
 bp = Blueprint('base', __name__, url_prefix='/')
 
-APP_VERSION = 'v0.1'
+APP_VERSION = 'v0.0.1'
 
 @bp.route('/')
 def query():
-    print('hello?')
     return redirect(url_for('flasgger.apidocs'))
